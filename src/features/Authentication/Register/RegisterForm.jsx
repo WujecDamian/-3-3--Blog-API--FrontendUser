ed39c3ac10e8de724c9fe3b0eeb7aee1c1376fc7
Damian Wujec <wujecdamian12@gmail.com>
2026-07-06 01:03:56 +0200

    feat: add register functionality with POST form (api fetch)

---
+++ src/features/Authentication/Register/RegisterForm.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import styles from "./RegisterForm.module.css";
+import { Navigate, useNavigate } from "react-router-dom";
+
+const RegisterForm = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e, name) => {
+    if (name === "email") {
+      setEmail(e.target.value);
+    } else if (name === "name") {
+      setName(e.target.value);
+    } else if (name === "password") {
+      setPassword(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const newUserData = {
+      email,
+      name,
+      password,
+    };
+    console.log(newUserData);
+    fetch("http://localhost:3000/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/JSON",
+      },
+      body: JSON.stringify(newUserData),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        setEmail("");
+        setName("");
+        setPassword("");
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label htmlFor="email">E-mail</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        value={email}
+        onChange={(e) => handleChange(e, "email")}
+      />
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        value={name}
+        onChange={(e) => handleChange(e, "name")}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        value={password}
+        onChange={(e) => handleChange(e, "password")}
+      />
+      <input type="submit" value="Register" />
+    </form>
+  );
+};
+
+export default RegisterForm;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,18 @@
+import RegisterForm from "../features/Authentication/Register/RegisterForm";
+import styles from "./Register.module.css";
+
 export default function Register() {
   return (
     <main>
       <h1>Hello</h1>
       <h2>I'm a register page</h2>
+      <section className={styles.register__info}>
+        <ul>
+          <li>Password must be at least 8 characters</li>
+          <li>Password must contain at least 1 capital letter & 1 number </li>
+        </ul>
+      </section>
+      <RegisterForm></RegisterForm>
     </main>
   );
 }
