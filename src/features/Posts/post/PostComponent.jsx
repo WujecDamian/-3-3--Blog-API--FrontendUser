c91c817ab21ddf0b5c69369225e4ab9a807cf8a8
Damian Wujec <wujecdamian12@gmail.com>
2026-07-05 23:05:30 +0200

    feat: truncate post content in PostComponent for better readability

--- src/features/Posts/post/PostComponent.jsx
+++ src/features/Posts/post/PostComponent.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
 const PostComponent = ({ post }) => {
+  const contentSnippet = post.content.toString().slice(0, 50);
+
   return (
     <Link to={`/post/${post.id}`}>
       <div className="post">
         <h2>{post.title}</h2>
-        <p>{post.content}</p>
+        <p>{contentSnippet}...</p>
         <span>{post.date}</span>
       </div>
     </Link>
