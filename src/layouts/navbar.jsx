a4e95df54ea192ea973098a267b1c606c8156cd1
Damian Wujec <wujecdamian12@gmail.com>
2026-07-07 20:11:14 +0200

    feat: implement conditional navbar links based on user login status (logged in or not)

--- src/layouts/navbar.jsx
+++ src/layouts/navbar.jsx
@@ -1,29 +1,41 @@
 import { Link } from "react-router-dom";
 import styles from "./navbar.module.css";
-import { User } from "../contexts/UserContext";
+import { User } from "../contexts/userContext";
 
 function NavBar() {
-  /*  const { user } = User();
-  if (user) {
-    const userObj = JSON.parse(user);
-  }
- */
+  const { user, logout } = User();
+
   return (
     <nav className={styles.navbar}>
       <Link to="/" className={styles.link}>
         Posts
       </Link>
-      <Link to="/log-in" className={styles.link}>
-        Log In
-      </Link>
-      <Link to="/register" className={styles.link}>
-        Register
-      </Link>
-      {/*       {user ? (
-        <p className={styles.username}>{userObj.name}</p>
+
+      {user ? (
+        <>
+          <Link
+            to="/log-out"
+            className={`${styles.link}, ${styles.last__link}`}
+            onClick={logout}
+          >
+            Log out
+          </Link>
+          <p className={styles.username}>{user.name}</p>
+        </>
       ) : (
-        <p className={styles.username}></p>
-      )} */}
+        <>
+          <Link to="/log-in" className={styles.link}>
+            Log In
+          </Link>
+          <Link
+            to="/register"
+            className={`${styles.link}, ${styles.last__link}`}
+          >
+            Register
+          </Link>
+          <p className={styles.username}></p>
+        </>
+      )}
     </nav>
   );
 }
